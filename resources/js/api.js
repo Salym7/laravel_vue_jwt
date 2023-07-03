8a3c100a96adf395a4151e3a1fe37bdd1ca274f8
Oleksandr Solianyk <salym3k@gmail.com>
2023-07-03 20:40:28 +0300

    Final

--- resources/js/api.js
+++ resources/js/api.js
@@ -7,9 +7,7 @@ const api = axios.create();
 api.interceptors.request.use(
     (config) => {
         if (localStorage.getItem("access_token")) {
-            config.headers = {
-                authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            };
+            config.headers.authorization = `Bearer ${localStorage.getItem("access_token")}`;
         }
         return config;
     },
@@ -17,21 +15,30 @@ api.interceptors.request.use(
 );
 // end request
 // start response
-api.interceptors.response.use(
-    (config) => {
-        if (localStorage.getItem("acces token")) {
-            config.headers = {
-                authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            };
-        }
-        return config;
-    },
-    (error) => {
-        if (error.response.status === 401) {
-            router.push({ name: "user.login" });
-        }
+api.interceptors.response.use({}, (error) => {
+    if (error.response.data.message === "Token has expired") {
+        return axios
+            .post(
+                "api/auth/refresh",
+                {},
+                {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem("access_token")}`,
+                    },
+                }
+            )
+            .then((res) => {
+                localStorage.setItem("access_token", res.data.access_token);
+
+                error.config.headers.authorization = `Bearer ${res.data.access_token}`;
+
+                return api.request(error.config);
+            });
     }
-);
+    if (error.response.status === 401) {
+        router.push({ name: "user.login" });
+    }
+});
 // end response
 
 export default api;
